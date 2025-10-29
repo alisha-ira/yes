a10e62669c386ab19141d3f4f93b7fc5c8087daf
alisha-ira <gamerzzblue1610@gmail.com>
2025-10-29 22:39:29 +0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -579,7 +579,7 @@ const calendarPosts = [...scheduledPosts, ...plannedPosts].map((post) => {
 
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mb-6">
               <div className="lg:col-span-2">
-                <Calendar
+               <Calendar
                   scheduledPosts={calendarPosts} // <-- use merged and fixed posts
                   onDateSelect={setSelectedDate}
                   onPostClick={handleEditScheduledPost}
